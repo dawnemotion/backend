0414f2726e5a7ac921f71cbf5ff7b338ab6534a1
dawnemotion <nextdodream@gmail.com>
2022-04-01 09:53:46 +0900

    a

--- algorithm/14/행렬의덧셈.js
+++ algorithm/14/행렬의덧셈.js
@@ -11,3 +11,15 @@ function solution(arr1, arr2) {
   }
   return answer;
 }
+
+function solution(arr1, arr2) {
+  //     배열을 순회하는 map메소드사용
+  //     반환은 새로운 배열을 반환한다.
+  //     배열을 반환하는 메소드를 2개사용하니 배열안에 배열이 존재하는 값을 반환한다.
+  const answer = arr1.map((num1, i) => {
+    return num1.map((num2, l) => {
+      return num2 + arr2[i][l];
+    });
+  });
+  return answer;
+}
